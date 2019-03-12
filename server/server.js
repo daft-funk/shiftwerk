2d18a3af0a430688043e6a7d59741885e09b0d33
aeginidae <aeginidae@gmail.com>
2019-03-12 13:03:45 -0500

    server exists

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-console.log("hello world!");
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
