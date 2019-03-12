9eb9ce62c3947a7c985413a5d3e76019c3edcb41
aeginidae <aeginidae@gmail.com>
2019-03-11 19:31:29 -0500

    import additional modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,10 @@ module.exports = {
         "test": "readonly",
         "expect": "readonly",
         "beforeAll": "readonly",
+        "jest": "readonly",
+        "beforeEach": "readonly",
+        "afterEach": "readonly",
+        "afterAll": "readonly",
+        "xdescribe": "readonly",
     },
 };
