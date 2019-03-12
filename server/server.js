cd808f315c767d08af2bcf6c53df02cc22ace432
aeginidae <aeginidae@gmail.com>
2019-03-12 13:28:40 -0500

    use nodemon, serve static assets

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('dist/Angular6App'));
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
