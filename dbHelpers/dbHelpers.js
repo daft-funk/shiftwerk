484c4ff55d3f828cc9404f4cda0c376905ef43ef
Taniecia Duplantis <duplantist@gmail.com>
2019-03-14 14:41:48 -0500

    acceptShift created

--- dbHelpers/dbHelpers.js
+++ dbHelpers/dbHelpers.js
@@ -1,11 +1,9 @@
-/* creating shifts
+/* 
   accepting shifts
   declining shifts
   applying to shifts
-  searching
-  for shifts
-  searching
-  for werkers
+  searching for shifts
+  searching for werkers
   inviting werkers to shift
   getting user profile
 */
@@ -14,11 +12,43 @@ const sequelize = require('sequelize');
 const db = require('../db/index');
 
 // function to create shifts
-const createShift = (name, time_date, duration, address, lat, long, payment_amnt, description, cache_rating) => {
+const createShift = (data) => {
   return db.models.Shift.create({
-    name: name, time_date: time_date, duration: duration, address: address, lat: lat, long: long, payment_amnt: payment_amnt, description: description, cache_rating: cache_rating 
+    name: data.name, time_date: data.time_date, duration: data.duration, address: data.address, lat: data.lat, long: data.long, payment_amnt: data.payment_amnt, description: data.description, cache_rating: data.cache_rating,
   });
 };
 
-// funciton to accept shifts
-const acceptShift = ()
+// function to accept shifts
+const acceptShift = (data) => {
+  return db.models.InviteApply.update({
+    status: 'Accepted',
+  }, {
+    where: {
+      wekerId: data.werkerId,
+      shiftId: data.shiftId,
+    },
+  });
+};
+
+// function to decline shifts
+const declineShift = () => {
+};
+
+// function to search for shifts
+const shiftSearch = () => {
+};
+
+// function to search for werkers
+const werkerSearch = () => {
+};
+
+// function to invite werkers to a shift
+const inviteWerkers = (data) => {
+  return db.models.InviteApply.create({ 
+    idWerker: data.idWerker, idShift: data.idWerker, idPosition: data.idPosition, status: data.status, expiration: data.expiration, type: data.type,
+  });
+};
+
+// function to get a user profile
+const getProfile = (userId) => {
+};
