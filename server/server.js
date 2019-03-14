20ae649e6e1cc5885378e325c31c1889530ef4d2
David <dlum988@gmail.com>
2019-03-13 20:11:20 -0500

    added cors and bodyparser

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
 app.use(express.static('dist/Angular6App'));
 
 const port = process.env.PORT || 4000;
