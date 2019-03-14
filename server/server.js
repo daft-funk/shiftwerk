854d5593b782dedf5dcc46cda7125450fe2fcd4e
David <dlum988@gmail.com>
2019-03-13 20:30:05 -0500

    fixing merges

--- server/server.js
+++ server/server.js
@@ -1,20 +1,17 @@
 const express = require('express');
-<<<<<<< HEAD
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = express();
 
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('dist/Angular6App'));
 
-=======
 const models = require('../db/index');
 
-const app = express();
 
->>>>>>> 3c03b7910133a1ca8ba8d794c4ae5425b7d13251
+
 const port = process.env.PORT || 4000;
 
 
