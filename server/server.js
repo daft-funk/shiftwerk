a81068ddd7edb28dd647539bc35d53b4ddf59b2f
David <dlum988@gmail.com>
2019-03-13 20:47:41 -0500

    added dotenv to server file

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -6,15 +7,17 @@ const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('dist/Angular6App'));
 
 const models = require('../db/index');
 
 
+app.get('/', (req, res) => {
+  res.send("I'm connected!");
+});
+
+
 
 const port = process.env.PORT || 4000;
-
-
 // models.sequelize.sync();
 
 app.listen(port, () => {
