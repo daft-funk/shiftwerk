6b0e56acbbcfe3468f62aca5dd2fc362bfdbe7c2
aeginidae <aeginidae@gmail.com>
2019-03-16 14:30:53 -0500

    linting

--- dbHelpers/dbHelpers.js
+++ dbHelpers/dbHelpers.js
@@ -1,6 +1,18 @@
 /* eslint-disable camelcase */
 // const sequelize = require('sequelize');
 const db = require('../db/index');
+
+/**
+ * Adds an array of items to Positions table or updates if they exist
+ * @param {Object} positions - key: position, val: string, key: payment_amnt, val: Number
+ */
+const bulkAddNewPositionsToShift = (shift, positions) => Promise.all(positions
+  .map(position => db.models.Position.upsert(position, { returning: true })
+    .then(([newPosition, updated]) => db.models.ShiftPosition.create({
+      ShiftId: shift.id,
+      PositionId: newPosition.id,
+      payment_amnt: position.payment_amnt,
+    }))));
 /**
  * Function used to create a new shift
  * @param {string} name - the name of the shift
@@ -9,7 +21,7 @@ const db = require('../db/index');
  * @param {number} lat - the latitude of the shift
  * @param {number} long - the longitude of the shift
  * @param {string} description - a short description of the shift
- * @param {Array<object>} positions - each position object has properties "position" and "payment_amnt"
+ * @param {Array<object>} positions - has properties "position" and "payment_amnt"
  * @param {string} payment_type - what format the pay is in, e.g. cash, digital, check
  */
 const createShift = ({
@@ -22,93 +34,44 @@ const createShift = ({
   description,
   positions,
   payment_type,
-}) => {
-  return db.models.Shift.create({
-    MakerId,
-    name,
-    time_date,
-    duration,
-    lat,
-    long,
-    description,
-    payment_type,
-  })
-    .then(newShift => {
-      return bulkAddNewPositionsToShift(newShift, positions);
-    })
-  // return bulkAddNewPositions(positions.map(position => ({position: position.position})))
-  //   .then((Positions) => {
-  //     return db.models.Shift.create({
-  //       MakerId,
-  //       name,
-  //       time_date,
-  //       duration,
-  //       lat,
-  //       long,
-  //       description,
-  //       payment_type,
-  //     }, {
-  //       include: Positions.slice(0, Positions.length - 1),
-  //     })
-      // .then((newShift) => {
-        // return newShift.setPositions(newPositions, { through: positions.map(position => ({ payment_amnt: position.payment_amnt }))});
-        // return Promise.all(newPositions.slice(0, newPositions.length - 1).map((position, index) => newShift.addPosition(position, { through: { payment_amnt: positions[index].payment_amnt } })));
-      // });
-    // });
-  // db.models.Shift.create({
-  //   MakerId,
-  //   name,
-  //   time_date,
-  //   duration,
-  //   lat,
-  //   long,
-  //   description,
-  // });
-};
-
-/**
- * Adds an array of items to Positions table or updates if they exist
- * @param {Object} positions - key: position, val: string, key: ShiftPosition: value: { payment_amnt: Number }
- */
-const bulkAddNewPositionsToShift = (shift, positions) => {
-  return Promise.all(positions.map(position => db.models.Position.upsert(position, { returning: true })
-    .then(([newPosition, updated]) => db.models.ShiftPosition.create({
-      ShiftId: shift.id,
-      PositionId: newPosition.id,
-      payment_amnt: position.payment_amnt,
-    }))));
-};
+}) => db.models.Shift.create({
+  MakerId,
+  name,
+  time_date,
+  duration,
+  lat,
+  long,
+  description,
+  payment_type,
+})
+  .then(newShift => bulkAddNewPositionsToShift(newShift, positions));
 
 /**
  * Function used to apply for a shift - updates the shift status to 'Pending'
  * @param {number} shiftId - the id of the shift to be applied to
  * @param {number} werkerId - the of the werker applying to the shift
  */
-const applyForShift = (shiftId, werkerId) => {
-  return db.models.InviteApply.update({
-    status: 'Pending',
-    where: {
-      shiftId,
-      werkerId,
-    },
-  });
-};
+const applyForShift = (shiftId, werkerId) => db.models.InviteApply.update({
+  status: 'Pending',
+  where: {
+    shiftId,
+    werkerId,
+  },
+});
 
 /**
  * Function used to accept shifts - updates the shift status to 'Accepted'
  * @param {number} shiftId - the id of the shift to be accepted
  * @param {number} werkerId - the id of the werker accepting the shift
  */
-const acceptShift = (shiftId, werkerId) => {
-  return db.models.InviteApply.update({
-    status: 'Accepted',
-  }, {
-    where: {
-      shiftId,
-      werkerId,
-    },
-  });
-};
+const acceptShift = (shiftId, werkerId) => db.models.InviteApply.update({
+  status: 'Accepted',
+}, {
+  where: {
+    shiftId,
+    werkerId,
+  },
+});
 
 /**
  * Function to decline shifts - updates the shift status to 'Declined'
@@ -129,58 +92,53 @@ const declineShift = (shiftId, werkerId) => db.models.InviteApply.update({
  * Function to search for shifts by keywords
  * @param {object} data - an object with search terms
  */
-const getShiftsBySearchTermsAndVals = (data) => {
-  return db.models.ShiftPosition.find({
-    where: { id: data.PositionId, payment_amnt: data.payment_amnt },
-    include: [db.models.Shift, db.models.Position],
-  });
-};
+const getShiftsBySearchTermsAndVals = data => db.models.ShiftPosition.find({
+  where: { id: data.PositionId, payment_amnt: data.payment_amnt },
+  include: [db.models.Shift, db.models.Position],
+});
 
 /**
  * Function to search for werkers by position
  * @param {object} data - an object with search terms
  */
-const getWerkersByTerm = (data) => {
-  return db.models.Werker.find({
-    where: { id: data.PositionId },
-    include: [db.models.Position],
-  });
-};
+const getWerkersByTerm = data => db.models.Werker.find({
+  where: { id: data.PositionId },
+  include: [db.models.Position],
+});
 
 /**
  * Function to invite a werker to a shift - creates a new invitation
  * @param {number} shiftId - the id of the shift that the werker will be invited to
  * @param {object} data - the data needed to create the new invitation
  */
-const inviteWerker = (shiftId, data) => {
-  return db.models.InviteApply.create({
-    idWerker: data.idWerker, idShift: shiftId, idPosition: data.idPosition, status: data.status, expiration: data.expiration, type: data.type,
-  });
-};
+const inviteWerker = (shiftId, data) => db.models.InviteApply.create({
+  idWerker: data.idWerker,
+  idShift: shiftId,
+  idPosition: data.idPosition,
+  status: data.status,
+  expiration: data.expiration,
+  type: data.type,
+});
 
 /**
  * Function to get a user profile
  * @param {object} data - object containing the user's id
  */
-const getProfile = (data) => {
-  return db.models.Werker.findOne({
-    where: {
-      id: data.id,
-    },
-  });
-};
+const getProfile = data => db.models.Werker.findOne({
+  where: {
+    id: data.id,
+  },
+});
 
 /**
  * Function to get a shift by it's id
  * @param {number} shiftId - id of the shift to be found
  */
-const getShiftsById = (shiftId) => {
-  return db.models.Shift.findOne({
-    where: {
-      id: shiftId,
-    },
-  });
-};
+const getShiftsById = shiftId => db.models.Shift.findOne({
+  where: {
+    id: shiftId,
+  },
+});
 
 /**
  * gets ten shifts from the DB, sorted by time_date
