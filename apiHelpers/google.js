e48f49eb9650fdeb0a34a021c42affaa80be83ca
aeginidae <aeginidae@gmail.com>
2019-03-27 13:58:34 -0500

    getGoogleProfile takes an oauth client as parameter

--- apiHelpers/google.js
+++ apiHelpers/google.js
@@ -1,52 +1,59 @@
 const { google } = require('googleapis');
 const { oauth2Client } = require('../auth/auth');
+const { addWerker } = require('../dbHelpers/dbHelpers');
+const { models } = require('../db');
 
-const people = google.people({
-  version: 'v1',
-  auth: oauth2Client,
-});
+// const people = google.people({
+//   version: 'v1',
+//   auth: oauth2Client,
+// });
 const calendar = google.calendar({
   version: 'v3',
   auth: oauth2Client,
 });
 
-const getGoogleProfile = (req, res, next) => {
-  // oauth2Client.setCredentials({
-  //   access_token: req.id,
-  //   refresh_token: '',
-  // });
-  console.log(req.user.google_id);
+const getGoogleProfile = (user, type, client) => {
+  const people = google.people({
+    version: 'v1',
+    auth: client,
+  });
   return people.people.get({
-    resourceName: `people/${req.user.google_id}`,
+    resourceName: `people/${user.sub}`,
     personFields: 'emailAddresses,names,photos,urls,phoneNumbers',
   })
     .then((googleRes) => {
       console.log(googleRes);
-      if (req.user.type === 'maker') {
-        req.user = Object.assign(req.user, {
+      if (type === 'maker') {
+        return {
           name: googleRes.data.names ? googleRes.data.names[0].displayName : '',
           email: googleRes.data.emailAddresses ? googleRes.data.emailAddresses[0].value : '',
           url_photo: googleRes.data.photos ? googleRes.data.photos[0].url : '',
           phone: googleRes.data.phoneNumbers ? googleRes.data.phoneNumbers[0].value : '', // this is a guess!
-        });
-      } else {
-        req.user = Object.assign(req.user, {
-          name_first: googleRes.data.names ? googleRes.data.names[0].givenName : '',
-          name_last: googleRes.data.names ? googleRes.data.names[0].familyName : '',
-          email: googleRes.data.emailAddresses ? googleRes.data.emailAddresses[0].value : '',
-          url_photo: googleRes.data.photos ? googleRes.data.photos[0].url : '',
-          phone: googleRes.data.phoneNumbers ? googleRes.data.phoneNumbers[0].value : '', // this is a guess!
-          bio: '',
-          certifications: [],
-          positions: [],
-        });
+        };
       }
-      next();
+      return {
+        name_first: googleRes.data.names ? googleRes.data.names[0].givenName : '',
+        name_last: googleRes.data.names ? googleRes.data.names[0].familyName : '',
+        email: googleRes.data.emailAddresses ? googleRes.data.emailAddresses[0].value : '',
+        url_photo: googleRes.data.photos ? googleRes.data.photos[0].url : '',
+        phone: googleRes.data.phoneNumbers ? googleRes.data.phoneNumbers[0].value : '', // this is a guess!
+        bio: '',
+        certifications: [],
+        positions: [],
+      };
     })
     .catch((err) => {
       console.error(err);
-      res.status(403).send('Unauthorized user');
     });
+};
+
+const saveGoogleProfile = (user, type) => {
+  if (type === 'maker') {
+    return models.Maker.upsert(user, {
+      returning: true,
+    }).spread(maker => maker);
+  }
+  return addWerker(user);
 };
 
 const addToCalendar = async (token) => {
@@ -81,4 +88,5 @@ module.exports = {
   getGoogleProfile,
   addToCalendar,
   removeFromCalendar,
+  saveGoogleProfile,
 };
